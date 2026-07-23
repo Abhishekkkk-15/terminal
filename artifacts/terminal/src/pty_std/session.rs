b9a4de15e29765d785756347ad91b843c7365a17
Abhishek Jangid <mrabhi748@gmail.com>
2026-07-23 16:37:29 +0000

    refactor: implement a TerminalManager struct which is only responsible to talking to the pty

--- artifacts/terminal/src/pty_std/session.rs
+++ artifacts/terminal/src/pty_std/session.rs
@@ -1,5 +1,5 @@
 use anyhow::{Ok, Result};
-use portable_pty::{CommandBuilder, PtyPair, PtySize, native_pty_system};
+use portable_pty::{Child, CommandBuilder, MasterPty, PtyPair, PtySize, native_pty_system};
 use std::io::{BufRead, BufReader, Read, Write};
 
 pub struct TerminalSession {
@@ -73,6 +73,68 @@ impl PtyWriter {
     pub fn write(&mut self, input: &str) -> Result<()> {
         self.writer.write_all(input.as_bytes())?;
         self.writer.flush()?;
+        Ok(())
+    }
+}
+
+struct TerminalManager {
+    master: Box<dyn MasterPty + Send>,
+    writer: Box<dyn Write + Send>,
+    reader: Box<dyn Read + Send>,
+    child: Box<dyn Child + Send>,
+}
+
+impl TerminalManager {
+    pub fn new(shell: &str) -> Result<Self> {
+        let pty_system = native_pty_system();
+
+        let pair = pty_system.openpty(PtySize {
+            rows: 24,
+            cols: 80,
+            pixel_width: 0,
+            pixel_height: 0,
+        })?;
+
+        let cmd = CommandBuilder::new(shell);
+        let child = pair.slave.spawn_command(cmd)?;
+
+        let reader = pair.master.try_clone_reader()?;
+        let writer = pair.master.take_writer()?;
+
+        Ok(Self {
+            master: pair.master,
+            reader,
+            writer,
+            child,
+        })
+    }
+
+    pub fn wirte(&mut self, bytes: &[u8]) -> Result<()> {
+        self.writer.write_all(&bytes)?;
+        self.writer.flush()?;
+        Ok(())
+    }
+
+    pub fn read(&mut self, buffer: &mut [u8]) -> Result<usize> {
+        Ok(self.reader.read(buffer)?)
+    }
+    pub fn resize(&self, rows: u16, cols: u16) -> Result<()> {
+        self.master.resize(PtySize {
+            rows,
+            cols,
+            pixel_width: 0,
+            pixel_height: 0,
+        });
+        Ok(())
+    }
+
+    pub fn kill(&mut self) -> Result<()> {
+        self.child.kill()?;
+        Ok(())
+    }
+
+    pub fn wait(&mut self) -> Result<()> {
+        self.child.wait()?;
         Ok(())
     }
 }
